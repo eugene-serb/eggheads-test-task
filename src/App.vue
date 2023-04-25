64aa70d708be2d4fc3d5947f8e65e10411c81909
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-04-25 15:00:34 +0300

    init vue 3

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {}
+  }
+</script>
+
+<style>
+  :root {
+    --font-family: Avenir, Helvetica, Arial, sans-serif;
+    --font-color: #000000;
+  }
+
+  *, ::after, ::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: var(--font-family);
+    color: var(--font-color);
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
