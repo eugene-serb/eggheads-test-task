f5b51ad98283167f878de95f2723b8982e3a4b73
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-04-26 10:58:51 +0300

    split app into components

--- src/App.vue
+++ src/App.vue
@@ -1,145 +1,22 @@
 ﻿<template>
-  <div id="table"></div>
+  <DataTable :products="products" />
 </template>
 
 <script>
-  /* eslint-disable no-undef */
+  import DataTable from '@/components/DataTable.vue';
   import products from '@/assets/data.json';
 
   export default {
     name: 'App',
-    components: {},
+    components: { 
+      DataTable,
+    },
     data() {
-      const layout = {
-        rows: [
-          {
-            view: 'template',
-            type: 'header',
-            template: 'Webix Lesson',
-          },
-          {
-            view: 'datatable',
-            autoConfig: true,
-            autowidth: false,
-            autoheight: true,
-            resizeColumn: true,
-            dragColumn: 'order',
-            rowHeight: 40,
-            data: products,
-            columns: [
-              {
-                id: 'image',
-                css: 'table__column',
-                header: [
-                  { text: 'Фото', css: 'table__header' },
-                ],
-                template: (item) => `<div class="table__image-container"><img class="table__image" src="/assets/thumbs/${item.image}" /></div>`,
-              },
-              {
-                id: 'sku',
-                sort: 'string',
-                css: 'table__column',
-                header: [
-                  { text: 'Артикул', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'schedule',
-                sort: 'string',
-                header: [
-                  { text: 'График продаж', css: 'table__header' },
-                ],
-                css: 'table__column',
-                template: webix.Sparklines.getTemplate({
-                  type: 'bar',
-                  width: 30,
-                }),
-                minWidth: 200,
-                width: 250,
-                maxWidth: 300,
-              },
-              {
-                id: 'name',
-                sort: 'string',
-                css: 'table__column',
-                header: [
-                  { text: 'Имя', css: 'table__header' },
-                ],
-                minWidth: 200,
-                width: 250,
-                maxWidth: 300,
-              },
-              {
-                id: 'brand',
-                sort: 'string',
-                css: 'table__column',
-                header: [
-                  { text: 'Бренд', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'merchant',
-                sort: 'string',
-                css: 'table__column',
-                header: [
-                  { text: 'Продавец', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'category',
-                sort: 'string',
-                css: 'table__column',
-                header: [
-                  { text: 'Группа', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'remain',
-                sort: 'int',
-                css: 'table__column',
-                header: [
-                  { text: 'Остаток', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'reviews',
-                sort: 'int',
-                css: 'table__column',
-                header: [
-                  { text: 'Отзывы', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'rating',
-                sort: 'int',
-                css: 'table__column',
-                header: [
-                  { text: 'Рейтинг', css: 'table__header' },
-                ],
-              },
-              {
-                id: 'cost',
-                sort: 'int',
-                css: 'table__column',
-                header: [
-                  { text: 'Цена', css: 'table__header' },
-                ],
-              },
-            ],
-          },
-        ],
-      };
-
       return {
-        layout,
+        products,
       };
     },
-    mounted() {
-      webix.ready(() => {
-        webix.ui(this.layout, this.$el);
-      });
-    }
-  }
+  };
 </script>
 
 <style>
@@ -164,34 +41,5 @@
     text-align: center;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-
-  #table {
-    height: 100vh;
-    width: 100vw;
-  }
-
-  .table__header {
-    font-size: var(--table-header-font-size);
-    font-weight: 500;
-  }
-
-  .table__column {
-    text-align: left;
-    font-size: var(--table-column-font-size);
-    line-height: var(--table-column-font-size);
-  }
-
-  .table__image-container {
-    width: 36px;
-    height: 36px;
-    margin: 4px 0 0 0;
-    padding: 0;
-    border-radius: 15px;
-    overflow: hidden;
-  }
-
-  .table__image {
-    width: 36px;
   }
 </style>
